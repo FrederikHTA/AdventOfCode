874d3352d729063071eea943c2248bf64d63808c
Frederik Træholt Andersen <fta@raptor.dk>
2021-12-04 13:32:40 +0100

    day3.2

--- src/main/scala/day3/Day3.scala
+++ src/main/scala/day3/Day3.scala
@@ -9,6 +9,7 @@ object Day3 extends App {
   val data = Source.fromResource("day3/data.txt").getLines.toList
 
   part1(data)
+  part2(data)
 
   def part1(input: List[String]): Unit = println {
     val binary = input
@@ -16,16 +17,50 @@ object Day3 extends App {
       .transpose
       .map(x => x.groupBy(identity).mapValues(_.size).maxBy(_._2)._1)
 
-    val res = binary.mkString("")
+    val res1 = binary.mkString("")
 
     val res2 = binary.map {
       case 1 => 0
       case 0 => 1
     }.mkString("")
 
-    val test = Integer.parseInt(res, 2)
+    val test = Integer.parseInt(res1, 2)
     val test2 = Integer.parseInt(res2, 2)
 
-    (s"res1 ${test * test2}")
+    s"res1: ${test * test2}"
+  }
+
+  def part2(input: List[String]): Unit = println{
+    val numbers = input
+      .map(_.split("").map(_.toInt).toList)
+
+    val oxygenGeneratorRatingBinary = numbers.transpose.foldLeft((numbers, 0))({
+      case ((input, iteration), _) =>
+        val test = input.transpose
+        val mostCommon = test(iteration).groupBy(identity).mapValues(_.size).maxBy(_._2)._1
+        val newList = input.filter(x => x(iteration) == mostCommon)
+        (newList, iteration + 1)
+    })._1.map(x => x.mkString("")).head
+
+    val oxygenGeneratorRating = Integer.parseInt(oxygenGeneratorRatingBinary, 2)
+
+    val co2ScrubberRatingBinary = numbers.transpose.foldLeft((numbers, 0))({
+      case ((input, iteration), _) =>
+        if (input.length == 1) (input, 0)
+        else {
+          val transposedInput = input.transpose
+          val entriesCount = transposedInput(iteration).groupBy(identity).mapValues(_.size)
+
+          val isEqualEntries = entriesCount.values.forall(_ == entriesCount.head._2)
+          val leastCommon = if (isEqualEntries) 0 else entriesCount.minBy(_._2)._1
+
+          val newList = input.filter(x => x(iteration) == leastCommon)
+          (newList, iteration + 1)
+        }
+    })._1.map(x => x.mkString("")).head
+
+    val co2ScrubberRating = Integer.parseInt(co2ScrubberRatingBinary, 2)
+
+    oxygenGeneratorRating * co2ScrubberRating
   }
 }
