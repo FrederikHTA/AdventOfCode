3747699295cd84901c728f9f1de6cc792c374be8
Frederik Træholt Andersen <fta@raptor.dk>
2021-12-07 23:14:21 +0100

    day 7 also with some help from reddit

--- src/main/scala/day7/Day7.scala
+++ src/main/scala/day7/Day7.scala
@@ -5,13 +5,21 @@ import scala.io.Source
 object Day7 {
   def part1(input: Seq[Int]): Int = {
     // get most frequent element
-    val alignPosition = input((input.length / 2).floor.toInt)
-
-    input.map(x => (x - alignPosition).abs).sum
+    val i = input.sorted.apply(input.size / 2)
+    calculateDistance(input, i).sum
   }
 
-  def part2(input: Seq[Int]): Unit = {
-    // TODO: implement
+  def part2(input: Seq[Int]): Int = {
+    (input.min to input.max).map(i => calculateDistance(input, i).map(calculateStepIncreases).sum).min
+  }
+
+  def calculateDistance(input: Seq[Int], n: Int): Seq[Int] = {
+    input.map(x => (x - n).abs)
+  }
+
+  // stolen from reddit meme
+  def calculateStepIncreases(x: Int): Int = {
+    (x * (x + 1)) / 2
   }
 
   def main(args: Array[String]): Unit = {
@@ -21,9 +29,7 @@ object Day7 {
       .split(",")
       .toSeq
       .map(_.toInt)
-      .sorted
 
-    println(input.mkString("Array(", ", ", ")"))
     println("Part1 result: " + part1(input))
     println("Part2 result: " + part2(input))
   }
