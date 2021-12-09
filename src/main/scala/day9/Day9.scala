85f94890595d9887e08454ca40be87bb9d445d8e
Frederik Træholt Andersen <fta@raptor.dk>
2021-12-09 23:34:28 +0100

    day9 part 2

--- src/main/scala/day9/Day9.scala
+++ src/main/scala/day9/Day9.scala
@@ -7,18 +7,48 @@ import scala.io.Source
 
 object Day9 {
   def part1(input: Grid[Int]): Int = {
-    val lowPoints = for {
+    val lowPoints = findLowPoints(input)
+
+    lowPoints.map(pos => input(pos) + 1).sum
+  }
+
+  def part2(input: Grid[Int]): Int = {
+    val lowPoints = findLowPoints(input)
+
+    val res = lowPoints.map(pos => {
+      findAllAdjacent(pos, input, Seq(pos))
+    })
+
+    res.sortBy(_.size).reverse.take(3).map(_.size).product
+  }
+
+
+  private def findAllAdjacent(pos: Pos, input: Grid[Int], basin: Seq[Pos]): Seq[Pos] = {
+    val newPos = pos
+      .getAdjacent
+      .filter(input.containsPos)
+      .filter(!basin.contains(_))
+      .filter(x => input(x) < 9)
+
+    val newBasin = newPos ++ basin
+
+    if(newPos.nonEmpty) {
+      newPos.foldLeft(newBasin)((newBasin, pos) => findAllAdjacent(pos, input, newBasin))
+    } else newBasin
+  }
+
+
+  private def findLowPoints(input: Grid[Int]): Vector[Pos] = {
+    for {
       (row, x) <- input.zipWithIndex
       (cell, y) <- row.zipWithIndex
       pos = Pos(x, y)
       adjacent = pos.getAdjacent.filter(input.containsPos)
       if adjacent.forall(pos => input(pos) > cell)
     } yield pos
-
-    lowPoints.map(pos => input(pos) + 1).sum
   }
 
-  def parseInput(input: String): Grid[Int] =
+  private def parseInput(input: String): Grid[Int] =
     input.linesIterator.map(_.split("").toVector.map(_.toInt)).toVector
 
   def main(args: Array[String]): Unit = {
@@ -28,5 +58,6 @@ object Day9 {
       .trim
 
     println(part1(parseInput(input)))
+    println(part2(parseInput(input)))
   }
 }
