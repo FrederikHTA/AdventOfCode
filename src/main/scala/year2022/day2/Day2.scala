dfc1b15007e79f1b37b8e6ec026ee9946719b1c5
Frederik Træholt Andersen <fta@raptor.dk>
2022-12-03 13:03:40 +0100

    very ugly day2

--- src/main/scala/year2022/day2/Day2.scala
+++ src/main/scala/year2022/day2/Day2.scala
@@ -1,60 +1,70 @@
 package year2022.day2
 
 import year2021.day15.Day15.getClass
+import year2022.day2
 
 import scala.collection.immutable.{AbstractSeq, LinearSeq}
 import scala.io.Source
+import RockPaperScissor._
 
-final case class Input(elfChoice: ElfChoice, myChoice: MyChoice) {
+final case class InputTuples(elfChoice: ElfChoice, myChoice: MyChoice)
 
-}
+enum RoundResult(value: Int):
+  def getValue() = value
+  case Loss extends RoundResult(0)
+  case Draw extends RoundResult(3)
+  case Win extends RoundResult(6)
 
 enum RockPaperScissor:
   case Rock, Paper, Scissor
 
-enum ElfChoice:
-  case A, B, C
+enum ElfChoice(choice: RockPaperScissor):
+  def getChoice() = choice
 
-enum MyChoice:
-  case X, Y, Z
+  case A extends ElfChoice(Rock)
+  case B extends ElfChoice(Paper)
+  case C extends ElfChoice(Scissor)
 
-/**
- * Elf Choices
- * A = Rock,
- * B = Paper,
- * C = Scissors
- */
+enum MyChoice(choice: RockPaperScissor, choiceValue: Int):
+  def getChoice() = choice
+  def getValue() = choiceValue
 
-/**
- * My Choices
- * X = Rock,
- * Y = Paper,
- * Z = Scissors
- */
+  case X extends MyChoice(Rock, 1)
+  case Y extends MyChoice(Paper, 2)
+  case Z extends MyChoice(Scissor, 3)
 
 object Day2 {
-  val loss = 0
-  val draw = 3
-  val win = 6
-
-  val rock = 1
-  val paper = 2
-  val scissor = 3
-
-  def part1(input: Seq[Input]): Int = {
-    val result = input.foldLeft(0) {
-      case (score, input) => {
-
-        score
-      }
-    }
-
-    result
+  def part1(input: Seq[InputTuples]): Int = {
+    val totalScore = input.foldLeft(0)(calculateWinner)
+    totalScore
   }
 
-  def parseInput(input: List[String]): Seq[Input] = {
+  def calculateWinner(score: Int, input: InputTuples): Int = {
+    val myChoice = input.myChoice.getChoice()
+    val elfChoice = input.elfChoice.getChoice()
+    score + getWinner(elfChoice, myChoice).getValue() + input.myChoice.getValue()
+  }
+
+  def getWinner(elfChoice: RockPaperScissor, myChoice: RockPaperScissor): RoundResult = {
+    if (myChoice == Rock && elfChoice == Paper)
+      return RoundResult.Loss
+    if (myChoice == Rock && elfChoice == Scissor)
+      return RoundResult.Win
+    if (myChoice == Paper && elfChoice == Rock)
+      return RoundResult.Win
+    if (myChoice == Paper && elfChoice == Scissor)
+      return RoundResult.Loss
+    if (myChoice == Scissor && elfChoice == Rock)
+      return RoundResult.Loss
+    if (myChoice == Scissor && elfChoice == Paper)
+      return RoundResult.Win
+
+    return  RoundResult.Draw
+  }
+
+  def parseInput(input: List[String]): Seq[InputTuples] = {
     val tuples = input.map(x => x.split(" "))
-    tuples.map(x => Input(ElfChoice.valueOf(x.head), MyChoice.valueOf(x.last)))
+    tuples.map(x => InputTuples(ElfChoice.valueOf(x.head), MyChoice.valueOf(x.last)))
   }
 
   def main(args: Array[String]): Unit = {
@@ -63,10 +73,8 @@ object Day2 {
       .getLines()
       .toList
 
-    val a = input.head
-    val b = input.last
-
     val part1Res = part1(parseInput(input))
     println(part1Res)
+    assert(part1Res == 10310)
   }
 }
---
+++ src/test/scala/Test2022.scala
@@ -0,0 +1,6 @@
+import org.scalatest.funsuite.AnyFunSuite
+import year2022.day1._
+
+class Test2022 extends AnyFunSuite {
+  Day1.main(Array())
+}
