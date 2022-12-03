7ac39687f9288052237e3c3f707bb6ea9603788a
Frederik Træholt Andersen <fta@raptor.dk>
2022-12-03 13:33:22 +0100

    somewhat prettified day 2, and solved part 2

--- src/main/scala/year2022/day2/Day2.scala
+++ src/main/scala/year2022/day2/Day2.scala
@@ -6,32 +6,38 @@ import year2022.day2
 import scala.collection.immutable.{AbstractSeq, LinearSeq}
 import scala.io.Source
 import RockPaperScissor._
+import RoundResult._
 
-final case class InputTuples(elfChoice: ElfChoice, myChoice: MyChoice)
+final case class InputTuples(elfChoice: Choice, myChoice: Choice)
 
 enum RoundResult(value: Int):
   def getValue() = value
+
   case Loss extends RoundResult(0)
   case Draw extends RoundResult(3)
   case Win extends RoundResult(6)
 
-enum RockPaperScissor:
-  case Rock, Paper, Scissor
+enum RockPaperScissor(value: Int):
+  def getValue() = value
 
-enum ElfChoice(choice: RockPaperScissor):
+  case Rock extends RockPaperScissor(1)
+  case Paper extends RockPaperScissor(2)
+  case Scissor extends RockPaperScissor(3)
+
+enum Choice(choice: RockPaperScissor, choiceValue: Int, desiredResult: RoundResult = Loss):
   def getChoice() = choice
 
-  case A extends ElfChoice(Rock)
-  case B extends ElfChoice(Paper)
-  case C extends ElfChoice(Scissor)
-
-enum MyChoice(choice: RockPaperScissor, choiceValue: Int):
-  def getChoice() = choice
   def getValue() = choiceValue
 
-  case X extends MyChoice(Rock, 1)
-  case Y extends MyChoice(Paper, 2)
-  case Z extends MyChoice(Scissor, 3)
+  def getDesiredResult() = desiredResult
+
+  case X extends Choice(Rock, 1, Loss)
+  case Y extends Choice(Paper, 2, Draw)
+  case Z extends Choice(Scissor, 3, Win)
+
+  case A extends Choice(Rock, -1)
+  case B extends Choice(Paper, -1)
+  case C extends Choice(Scissor, -1)
 
 object Day2 {
   def part1(input: Seq[InputTuples]): Int = {
@@ -39,32 +45,64 @@ object Day2 {
     totalScore
   }
 
-  def calculateWinner(score: Int, input: InputTuples): Int = {
-    val myChoice = input.myChoice.getChoice()
+  def part2(input: Seq[InputTuples]): Int = {
+    val totalScore = input.foldLeft(0)(calculateDesiredOutcome)
+    totalScore
+  }
+
+  def calculateDesiredOutcome(score: Int, input: InputTuples): Int = {
     val elfChoice = input.elfChoice.getChoice()
-    score + getWinner(elfChoice, myChoice).getValue() + input.myChoice.getValue()
+    val desiredResult = input.myChoice.getDesiredResult()
+    val myChoice = getDesiredOutcome(elfChoice, desiredResult).getValue()
+    score + desiredResult.getValue() + myChoice
+  }
+
+  def getDesiredOutcome(elfChoice: RockPaperScissor, myChoice: RoundResult): RockPaperScissor = {
+    myChoice match {
+      case Loss => elfChoice match {
+        case Rock => Scissor
+        case Paper => Rock
+        case Scissor => Paper
+      }
+      case Win => elfChoice match {
+        case Rock => Paper
+        case Paper => Scissor
+        case Scissor => Rock
+      }
+      case Draw => elfChoice
+    }
+  }
+
+  def calculateWinner(score: Int, input: InputTuples): Int = {
+    val elfChoice = input.elfChoice.getChoice()
+    val myChoice = input.myChoice.getChoice()
+    val roundResult = getWinner(elfChoice, myChoice).getValue()
+    score + roundResult + input.myChoice.getValue()
   }
 
   def getWinner(elfChoice: RockPaperScissor, myChoice: RockPaperScissor): RoundResult = {
-    if (myChoice == Rock && elfChoice == Paper)
-      return RoundResult.Loss
-    if (myChoice == Rock && elfChoice == Scissor)
-      return RoundResult.Win
-    if (myChoice == Paper && elfChoice == Rock)
-      return RoundResult.Win
-    if (myChoice == Paper && elfChoice == Scissor)
-      return RoundResult.Loss
-    if (myChoice == Scissor && elfChoice == Rock)
-      return RoundResult.Loss
-    if (myChoice == Scissor && elfChoice == Paper)
-      return RoundResult.Win
+    if (elfChoice.getValue() == myChoice.getValue())
+      return RoundResult.Draw
 
-    return  RoundResult.Draw
+    myChoice match {
+      case Rock => elfChoice match {
+        case Paper => Loss
+        case Scissor => Win
+      }
+      case Paper => elfChoice match {
+        case Rock => Win
+        case Scissor => Loss
+      }
+      case Scissor => elfChoice match {
+        case Rock => Loss
+        case Paper => Win
+      }
+    }
   }
 
   def parseInput(input: List[String]): Seq[InputTuples] = {
     val tuples = input.map(x => x.split(" "))
-    tuples.map(x => InputTuples(ElfChoice.valueOf(x.head), MyChoice.valueOf(x.last)))
+    tuples.map(x => InputTuples(Choice.valueOf(x.head), Choice.valueOf(x.last)))
   }
 
   def main(args: Array[String]): Unit = {
@@ -76,5 +114,9 @@ object Day2 {
     val part1Res = part1(parseInput(input))
     println(part1Res)
     assert(part1Res == 10310)
+
+    val part2Res = part2(parseInput(input))
+    println(part2Res)
+    assert(part2Res == 14859)
   }
 }
