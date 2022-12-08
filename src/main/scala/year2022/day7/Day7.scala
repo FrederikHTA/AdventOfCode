02c766eb760adaa72ae103826f910f5de525523e
Frederik Træholt Andersen <fta@raptor.dk>
2022-12-08 21:38:45 +0100

    formartting

--- src/main/scala/year2022/day7/Day7.scala
+++ src/main/scala/year2022/day7/Day7.scala
@@ -16,43 +16,53 @@ final case class Directory() extends Node
 final case class DirectoryContext(name: String, parent: String, nodes: Map[String, Node])
 
 object Day7 {
-  private val dir = """dir (\w+)""".r
-  private val file = """(\d+) (.+)""".r
-  private val cd = """\$ cd (\w+|\d+|\/)""".r
+  private val dir     = """dir (\w+)""".r
+  private val file    = """(\d+) (.+)""".r
+  private val cd      = """\$ cd (\w+|\d+|\/)""".r
   private val cdSlash = """\$ cd \/""".r
-  private val cdOut = """\$ cd \.\.""".r
-  private val ls = """\$ ls""".r
+  private val cdOut   = """\$ cd \.\.""".r
+  private val ls      = """\$ ls""".r
 
   def part1(nodes: Map[String, Node]) = {
-    val directories = nodes.filter(x => x._2 match {
-      case file: File => false
-      case dir: Directory => true
-    })
+    val directories = nodes.filter(x =>
+      x._2 match {
+        case file: File     => false
+        case dir: Directory => true
+      }
+    )
 
-    val sums = directories.map(key => nodes
-      .view
-      .filterKeys(x => x.startsWith(key._1))
-      .map(_._2)
-      .filter(x => x match {
-        case _: File => true
-        case _: Directory => false
-      })
-      .map(x => x.asInstanceOf[File].size).sum)
+    val sums = directories.map(key =>
+      nodes.view
+        .filterKeys(x => x.startsWith(key._1))
+        .map(_._2)
+        .filter(x =>
+          x match {
+            case _: File      => true
+            case _: Directory => false
+          }
+        )
+        .map(x => x.asInstanceOf[File].size)
+        .sum
+    )
+
+    val hej = sums.sum
 
     sums.filter(_ < 100_000).sum
   }
 
   def parseInput(input: Seq[String]) = {
-    input.foldLeft(DirectoryContext("", "", Map.empty[String, Node]))(buildTree).nodes
+    val emptyDirectory = DirectoryContext("", "", Map.empty[String, Node])
+    input.foldLeft(emptyDirectory)(buildTree).nodes
   }
 
   def buildTree(context: DirectoryContext, inputString: String): DirectoryContext = {
-    val delimiter = "$"
+    val delimiter = "_"
+
     val DirectoryContext(contextName, contextParent, contextNodes) = context
 
     inputString match {
       case cdSlash() => context
-      case ls() => context
+      case ls()      => context
       case dir(name) =>
         val updatesNodes = contextNodes + (s"$contextName$delimiter$name" -> Directory())
         DirectoryContext(contextName, contextParent, updatesNodes)
@@ -60,7 +70,7 @@ object Day7 {
         val updatesNodes = contextNodes + (s"$contextName$delimiter$name" -> File(size.toInt))
         DirectoryContext(contextName, contextParent, updatesNodes)
       case cd(name) =>
-        val newName = s"$contextName$delimiter$name"
+        val newName    = s"$contextName$delimiter$name"
         val newCurrent = contextNodes(newName)
         DirectoryContext(newName, contextName, contextNodes)
       case cdOut() =>
@@ -72,12 +82,12 @@ object Day7 {
 
   def main(args: Array[String]): Unit = {
     val input = Source
-      .fromInputStream(getClass.getResourceAsStream("data.txt"))
+      .fromInputStream(getClass.getResourceAsStream("testdata.txt"))
       .getLines
       .toSeq
 
     val parsedInput = parseInput(input)
-    val part1Res = part1(parsedInput)
+    val part1Res    = part1(parsedInput)
     println(part1Res)
     //    assert(part1Res == )
   }
