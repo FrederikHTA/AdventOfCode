a2b8cd77ceabc2b70cf9bddfafd3c6515c61833b
Frederik Harpøth Træholt <ftr@mft-energy.com>
2023-08-03 00:44:10 +0200

    day 11 part 1

---
+++ 2022-csharp/Template.cs
@@ -0,0 +1,18 @@
+using _2022_csharp.Lib;
+
+namespace _2022_csharp;
+
+static class Template
+{
+    public static void Part1()
+    {
+        var input = Utilities.GetLines("/FOLDERNAME/TestData.txt");
+
+        // result.Should().HaveCount(6314);
+        // Console.WriteLine("Result: " + result);
+    }
+    
+    public static void Part2()
+    {
+    }
+}
