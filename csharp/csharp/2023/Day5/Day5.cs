881fcec15568fd3ed2785c2c073b3c711e4fc35b
Frederik Harpøth Træholt <ftr@mft-energy.com>
2023-12-10 15:02:19 +0100

    day5 part2

--- csharp/csharp/2023/Day5/Day5.cs
+++ csharp/csharp/2023/Day5/Day5.cs
@@ -29,6 +29,7 @@ static class Day5
                 }).ToList()).ToList();
 
         var lowestLocation = new List<long>();
+        var lowestLocation2 = long.MaxValue;
         foreach (var t in seeds)
         {
             var currentValue = t;
@@ -41,7 +42,7 @@ static class Day5
                     {
                         continue;
                     }
-                    
+
                     var diff = row.Destination - row.Source;
                     var res = diff + currentValue;
                     currentValue = res;
@@ -49,14 +50,73 @@ static class Day5
                 }
             }
 
+
             lowestLocation.Add(currentValue);
         }
 
         var result = lowestLocation.Min();
         result.Should().Be(157211394);
+        Console.WriteLine(lowestLocation2);
     }
 
     public static void Part2()
     {
+        var input = File.ReadAllText(Directory.GetCurrentDirectory() + "/2023/Day5/Data.txt");
+        var lines = input.Split("\r\n\r\n")
+            .Select(x => x.Split("\r\n"))
+            .ToList();
+
+        var seeds = lines[0][0].Split(":")[1]
+            .Split(" ")
+            .Where(x => x != "")
+            .Select(long.Parse)
+            .ToList()
+            .Chunk(2)
+            .ToList();
+
+        var maps = lines.Skip(1)
+            .Select(group => group.Skip(1)
+                .Select(x =>
+                {
+                    var mapValues = x.Split(" ").Where(y => y != "").Select(long.Parse).ToList();
+                    return new Map(mapValues[0], mapValues[1], mapValues[2]);
+                }).ToList()).ToList();
+
+        var lowestLocation = long.MaxValue;
+        Console.WriteLine(seeds.Count);
+        // takes 10 ish minutes to run, could be optimized
+        for (var seedIndex = 0; seedIndex < seeds.Count; seedIndex++)
+        {
+            var t = seeds[seedIndex];
+            for (var i = t[0]; i < t[0] + t[1]; i++)
+            {
+                var currentValue = i;
+                foreach (var currentMap in maps)
+                {
+                    foreach (var row in currentMap)
+                    {
+                        var upperLimit = row.Source + row.Distance;
+                        if (row.Source > currentValue || currentValue > upperLimit)
+                        {
+                            continue;
+                        }
+
+                        var diff = row.Destination - row.Source;
+                        var res = diff + currentValue;
+                        currentValue = res;
+                        break;
+                    }
+                }
+
+                if (currentValue < lowestLocation)
+                {
+                    lowestLocation = currentValue;
+                }
+            }
+
+            Console.WriteLine($"Finished with seed group {seedIndex} out of {seeds.Count}");
+        }
+
+        Console.WriteLine(lowestLocation);
     }
 }
