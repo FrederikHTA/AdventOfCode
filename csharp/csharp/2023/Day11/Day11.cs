bd40994417f3b9e2ce17e98978a9ef1f1af03fcc
Frederik Harpøth Træholt <ftr@mft-energy.com>
2023-12-12 18:42:13 +0100

    day11 part2

--- csharp/csharp/2023/Day11/Day11.cs
+++ csharp/csharp/2023/Day11/Day11.cs
@@ -9,30 +9,89 @@ static class Day11
 {
     public static void Part1()
     {
+        const int distance = 2;
         var lines = Utilities.GetLines("/2023/Day11/Data.txt");
         var linesAsCharArray = lines.Select(x => x.ToCharArray().ToList()).ToList();
-        var grid = new Grid<char>(linesAsCharArray).Transpose();
+        var grid = new Grid<char>(linesAsCharArray);
 
-        var universe = ExpandRows(ExpandRows(grid).Transpose());
-        var galaxyPositions = FindGalaxyPositions(universe);
+        var galaxyPositions = FindGalaxyPositions(grid);
+
+        AddX(grid, galaxyPositions, distance);
+
+        AddY(grid, galaxyPositions, distance);
+
         var pairs = GenerateUniquePairs(galaxyPositions);
 
-        pairs.Select(x => x.Item1.ManhattanDistance(x.Item2))
+        pairs.Select(x => Math.Abs(x.Item1.Item1 - x.Item2.Item1) + Math.Abs(x.Item1.Item2 - x.Item2.Item2))
             .Sum()
             .Should()
             .Be(10077850);
     }
 
-    private static List<Pos> FindGalaxyPositions(Grid<char> universe)
+    private static void AddY(Grid<char> grid, List<(long, long)> galaxyPositions, int distance)
     {
-        var galaxyPositions = new List<Pos>();
+        for (var i = grid.Data[0].Count - 1; i > 0; i--)
+        {
+            if (grid.Data.TrueForAll(x => x[i] == '.'))
+            {
+                var toIncrease = galaxyPositions.FindAll(x => x.Item2 > i);
+                foreach (var pos in toIncrease)
+                {
+                    var newPos = pos with { Item2 = pos.Item2 + distance - 1 };
+                    galaxyPositions.Remove(pos);
+                    galaxyPositions.Add(newPos);
+                }
+            }
+        }
+    }
+
+    private static void AddX(Grid<char> grid, List<(long, long)> galaxyPositions, int distance)
+    {
+        for (var i = grid.Data.Count - 1; i > 0; i--)
+        {
+            if (grid.Data[i].TrueForAll(x => x == '.'))
+            {
+                var toIncrease = galaxyPositions.FindAll(x => x.Item1 > i);
+                foreach (var pos in toIncrease)
+                {
+                    var newPos = pos with { Item1 = pos.Item1 + distance - 1 };
+                    galaxyPositions.Remove(pos);
+                    galaxyPositions.Add(newPos);
+                }
+            }
+        }
+    }
+
+    public static void Part2()
+    {
+        const int distance = 1000000;
+        var lines = Utilities.GetLines("/2023/Day11/Data.txt");
+        var linesAsCharArray = lines.Select(x => x.ToCharArray().ToList()).ToList();
+        var grid = new Grid<char>(linesAsCharArray);
+
+        var galaxyPositions = FindGalaxyPositions(grid);
+
+        AddX(grid, galaxyPositions, distance);
+        AddY(grid, galaxyPositions, distance);
+
+        var pairs = GenerateUniquePairs(galaxyPositions);
+
+        pairs
+            .Sum(x => Math.Abs(x.Pos1.Item1 - x.Pos2.Item1) + Math.Abs(x.Pos1.Item2 - x.Pos2.Item2))
+            .Should()
+            .Be(504715068438);
+    }
+
+    private static List<(long, long)> FindGalaxyPositions(Grid<char> universe)
+    {
+        var galaxyPositions = new List<(long, long)>();
         for (var row = 0; row < universe.Data.Count; row++)
         {
             for (var col = 0; col < universe.Data[0].Count; col++)
             {
                 if (universe.Data[row][col] == '#')
                 {
-                    galaxyPositions.Add(new Pos(row, col));
+                    galaxyPositions.Add((row, col));
                 }
             }
         }
@@ -40,38 +99,17 @@ static class Day11
         return galaxyPositions;
     }
 
-    private static List<Tuple<Pos, Pos>> GenerateUniquePairs(List<Pos> galaxyPositions)
+    private static List<((long, long) Pos1, (long, long) Pos2)> GenerateUniquePairs(List<(long, long)> galaxyPositions)
     {
-        var pairs = new List<Tuple<Pos, Pos>>();
+        var pairs = new List<((long, long) Pos1, (long, long) Pos2)>();
         for (var i = 0; i < galaxyPositions.Count - 1; i++)
         {
             for (var j = i + 1; j < galaxyPositions.Count; j++)
             {
-                pairs.Add(Tuple.Create(galaxyPositions[i], galaxyPositions[j]));
+                pairs.Add((galaxyPositions[i], galaxyPositions[j]));
             }
         }
 
         return pairs;
-    }
-
-    private static Grid<char> ExpandRows(Grid<char> grid)
-    {
-        var res = new List<List<char>>();
-        var newRow = Enumerable.Repeat('.', grid.Data[0].Count).ToList();
-        foreach (var row in grid.Data)
-        {
-            if (row.TrueForAll(x => x == '.'))
-            {
-                res.Add(newRow);
-            }
-
-            res.Add(row);
-        }
-
-        return new Grid<char>(res);
-    }
-
-    public static void Part2()
-    {
     }
 }
