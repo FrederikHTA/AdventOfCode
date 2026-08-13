56056ce4666e53adcccbaffb9a1e3549e859b051
Frederik Træholt <frederikhta@gmail.com>
2026-08-13 12:25:50 +0200

    remove unoptimized version

--- dotnet/csharp/Custom/Troels1.cs
+++ dotnet/csharp/Custom/Troels1.cs
@@ -15,56 +15,6 @@ public class Troels1
      * - in "ab", {"a" -> "b", "b" -> "c"}
      * - out: "cc"
      */
-    public static void Troels1Solution()
-    {
-        var watch = new Stopwatch();
-        watch.Start();
-        /***
-         * Assumptions:
-         * - No loops in replacements
-         * - No overlapping replacements, i.e. "f" -> "b" and "foo" -> "bar" is not allowed, as f is a subset of foo
-         * - There are no sentences, only single words
-         * - If 2 words are of equals length, return the first entry
-         */
-        var input = new[] { "ab", "bc", "foo", "foobar" };
-        var replacements = new Dictionary<string, string>
-        {
-            { "a", "b" },
-            { "b", "c" },
-            { "foo", "bar" }
-        };
-
-        var maxReplacementsString = "";
-        foreach (string s in input)
-        {
-            var replacementCount = 0;
-            var tempString = "";
-            foreach (char character in s)
-            {
-                var currentReplacement = character.ToString();
-                while (replacements.TryGetValue(currentReplacement, out string? value))
-                {
-                    replacementCount++;
-                    currentReplacement = value;
-                }
-                tempString += currentReplacement;
-
-                while (replacements.TryGetValue(tempString, out string? value))
-                {
-                    replacementCount++;
-                    tempString = value;
-                }
-            }
-
-            if (replacementCount > maxReplacementsString.Length)
-            {
-                maxReplacementsString = tempString;
-            }
-        }
-        watch.Stop();
-        Console.WriteLine($"String with max replacements: {maxReplacementsString}, runTime: {watch.Elapsed}");
-    }
-
     public static void Troels1SolutionWithPreOptimizations()
     {
         var watch = new Stopwatch();
