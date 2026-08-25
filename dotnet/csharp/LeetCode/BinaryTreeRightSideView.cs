02394d0d5503e4a57f8b29f3ba0185806c615648
Frederik Træholt <frederikhta@gmail.com>
2026-08-25 14:26:58 +0200

    more

---
+++ dotnet/csharp/LeetCode/BinaryTreeRightSideView.cs
@@ -0,0 +1,42 @@
+﻿namespace csharp.LeetCode;
+
+public class BinaryTreeRightSideView
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode? left;
+        public TreeNode? right;
+
+        public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public static void Run()
+    {
+        // Input: root = [1,2,3,null,5,null,4]
+        // Output: [1,3,4]
+        // var result = RightSideView(root);
+        // Console.WriteLine(result);
+    }
+
+    public static List<int> RightSideView(TreeNode root)
+    {
+        var result = new List<int>();
+        Process(root, result, 0);
+        return result;
+    }
+
+    private static void Process(TreeNode node, List<int> result, int depth)
+    {
+        if (node is null) return;
+        
+        if(depth == result.Count) result.Add(node.val);
+        Process(node.right, result, depth + 1);
+        Process(node.left, result, depth + 1);
+    }
+}
---
+++ dotnet/csharp/LeetCode/PascalsTriangle.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace csharp.LeetCode;
+
+public class PascalsTriangle
+{
+    public static void Run()
+    {
+        // Input: numRows = 5
+        // Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+        const int input = 5;
+        var result = Generate(input);
+        foreach (var row in result)
+        {
+            Console.WriteLine(string.Join(" ", row));
+        }
+    }
+
+    private static IList<IList<int>> Generate(int numRows)
+    {
+        var result = new List<IList<int>>(numRows);
+
+        for (var row = 0; row < numRows; row++)
+        {
+            var depth = row + 1;
+            result.Add(new List<int>(depth));
+            for (var col = 0; col < depth; col++)
+            {
+                if (col == 0 || col == depth - 1)
+                {
+                    result[row].Add(1);
+                    continue;
+                }
+
+                var rowAbove = result[row - 1];
+                var left = rowAbove[col - 1];
+                var right = rowAbove[col];
+                result[row].Add(left + right);
+            }
+        }
+
+        return result;
+    }
+}
