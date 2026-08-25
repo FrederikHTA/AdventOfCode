42c99bd19257a6ac7d4db8d019afc868a6332890
Frederik Træholt <frederikhta@gmail.com>
2026-08-25 13:07:11 +0200

    PermutationInString

--- dotnet/csharp/LeetCode/LetterCombinationsOfAPhoneNumber.cs
+++ dotnet/csharp/LeetCode/LetterCombinationsOfAPhoneNumber.cs
@@ -19,8 +19,8 @@ public class LetterCombinationsOfAPhoneNumber
 
     private static readonly Dictionary<char, string> LetterMap = new()
     {
-        {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"},
-        {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"},
+        { '2', "abc" }, { '3', "def" }, { '4', "ghi" }, { '5', "jkl" },
+        { '6', "mno" }, { '7', "pqrs" }, { '8', "tuv" }, { '9', "wxyz" },
     };
 
     private static List<string> LetterCombinations(string digits)
---
+++ dotnet/csharp/LeetCode/PermutationInString.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace csharp.LeetCode;
+
+public class PermutationInString
+{
+    public static void Run()
+    {
+        // Input: s1 = "ab", s2 = "eidbaooo"
+        // Output: true
+        // Explanation: s2 contains one permutation of s1 ("ba").
+        const string s1 = "ab";
+        const string s2 = "eidbaooo";
+        // const string s2 = "aabb";
+        var result = CheckInclusion(s1, s2);
+        Console.WriteLine(result);
+    }
+
+    private static bool CheckInclusion(string s1, string s2)
+    {
+        var need = new int[26];
+        var window = new int[26];
+        foreach (var c in s1) need[c - 'a']++; 
+        
+        var matches = 0;
+
+        for (var i = 0; i < s2.Length - 1; i++)
+        {
+            var current = s2[i] - 'a';
+            window[current]++;
+            if (window[i] == need[i]) matches++;
+            if (window[i] > need[i]) matches--;
+
+            if (i > s1.Length)
+            {
+                var evict = s2[i - s1.Length] - 'a';
+                window[evict]--;
+                if (window[evict] == need[evict]) matches++;
+            }
+
+            if (matches == s1.Length) return true;
+        }
+
+        return false;
+    }
+}
